0e53f33908326bc6ef1f60d2ef3677d50ff8a03d
Daniel D <leftpolaright@gmail.com>
2016-12-09 14:19:58 +0200

    Finish configuration bank updater api endpoint

--- routes/updaters/configurationBankUpdater.es6.js
+++ routes/updaters/configurationBankUpdater.es6.js
@@ -1,5 +1,32 @@
+let FinancialState = require('../../models/financialState');
+
 module.exports = class ConfigurationBankUpdater {
+	constructor(logger) {
+		this.logger = logger;
+		this.FinancialState = FinancialState;
+	}
+
 	update(request, response, next) {
-		response.status(200).json({"message": "Success"});
+		this.FinancialState.findOne({ 'owner': 'all' }, (err, financialState) => {
+			if (err) next(err);
+			if (financialState) {
+				let financialStateConfiguration = financialState.configuration || {};
+				let financialStateBanksConfiguration = financialStateConfiguration.banks || [];
+				financialStateBanksConfiguration.push(request.body);
+				console.log(financialStateConfiguration);
+
+				FinancialState.findByIdAndUpdate(financialState.id, { $set: { "configuration.banks": financialStateBanksConfiguration } },
+					(updateError, updateDocument) => {
+						if (updateError) {
+							response.status(500).json(error);
+						}
+						response.status(200).json(updateDocument);
+					});
+			} else {
+				response.status(500).json({ "error": "Could not find financial state with owner of 'all'" });
+			}
+		});
+
+
 	}
 }
