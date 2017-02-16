8478aef8a0961f4287a4e71ff6f720f597391691
Daniel D <leftpolaright@gmail.com>
2017-02-16 19:55:26 +0200

    starting over the gulp file

--- public/scripts/financial_state/financialStateBootstrap.js
+++ public/scripts/financial_state/financialStateBootstrap.js
@@ -1,7 +1,22 @@
-import {FinancialStateController} from "financial_state/financialStateController";
-import {StatusFetchingService} from "services/statusFetchingService";
+// import {FinancialStateController} from "financial_state/financialStateController";
+// import {StatusFetchingService} from "services/statusFetchingService";
  
-let statusFetchingService = new StatusFetchingService();        
-let financialStateController = new FinancialStateController(statusFetchingService);  
-financialStateController.bindFetchingStatusForBank(jQuery('.bhp-get-status'), jQuery('.bhp-status-panel'), "BankHapoalim");
-financialStateController.bindFetchingStatusForBank(jQuery('.dis-get-status'), jQuery('.dis-status-panel'), "BankDiscount");    
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+
+const App = () => (
+  <div>
+    <div>Hello world</div>
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+// let statusFetchingService = new StatusFetchingService();        
+// let financialStateController = new FinancialStateController(statusFetchingService);  
+// financialStateController.bindFetchingStatusForBank(jQuery('.bhp-get-status'), jQuery('.bhp-status-panel'), "BankHapoalim");
+// financialStateController.bindFetchingStatusForBank(jQuery('.dis-get-status'), jQuery('.dis-status-panel'), "BankDiscount");    
