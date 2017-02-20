ec69cae30892a19b0b85194858e37bfb462facc9
Daniel D <leftpolaright@gmail.com>
2017-02-20 23:34:11 +0200

    adding all the textfields for all the necessary inputs

--- public/scripts/finance_control/financeControlApp.js
+++ public/scripts/finance_control/financeControlApp.js
@@ -8,7 +8,17 @@ import TextField from '../components/inputs/TextField/textField';
          
 const App = () => (
   <Card headerText="Input your current financial situation: ">
-    <TextField type="text" placeholderText="Label here"/>
+    <TextField type="text" placeholderText="BankHapoalim (p1) Total"/>
+    <TextField type="text" placeholderText="BankHapoalim (p1) Credit"/>
+    <TextField type="text" placeholderText="Discount Bank (p2) Total"/>
+    <TextField type="text" placeholderText="Discount Bank (p2) Credit"/>
+    <TextField type="text" placeholderText="FlyCard (p1) Credit"/>
+    <TextField type="text" placeholderText="FlyCard (p2) Credit"/>
+    <TextField type="text" placeholderText="Leumi (p1) Credit"/>
+    <TextField type="text" placeholderText="Lifestyle (p2) Credit"/>
+    <TextField type="text" placeholderText="Corporate (p1) Credit"/>
+    <TextField type="text" placeholderText="Other (p1) Credit"/>
+    <TextField type="text" placeholderText="Other (p2) Credit"/>
   </Card>
 ); 
 //  asdasd                
